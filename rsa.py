04d5e7fb0ec99bbe3af7cef358fa4630aa2ddcc5
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-12-29 23:24:34 +0000

    Add files via upload

---
+++ rsa.py
@@ -0,0 +1,8 @@
+from secrets import randbits
+from miller_rabin import miller_rabin
+
+def generate_prime(bits):
+    while True:
+        n = randbits(bits)
+        if n | 1 == n and miller_rabin(n):
+            return n     
