d24694ba7cf1353b554634290b0f24872a68eadc
deedy5 <65482418+deedy5@users.noreply.github.com>
2021-01-01 20:20:36 +0000

    Update rsa_keys() function in rsa.py
    Update rsa_keys() function

--- rsa.py
+++ rsa.py
@@ -11,31 +11,44 @@ def getprime(bits: int) -> int:
         k = 5
     elif bits == 1536:
         k = 4
-    else:
-        k = 10        
     while True:
         n = randbits(bits) | 1  # n += 1 if n is odd
-        if miller_rabin(n, k + 1):
-            return n
+        if n.bit_length() in (1024, 1536):
+            if miller_rabin(n, k + 1):
+                return n
 
 def rsa_keys(bits: int) -> (int, int):
-    """Get public and private keys"""
+    """Get public and private keys.
 
-    E = 65537
+    Method: FIPS 186-4, Appendix B.3.3.
+    Using these method p, q may be generated with lengths of 1024 or 1536 bits
+    (512 bits shall not). FIPS186-4. Appendix B.3.1. Criteria for IFC Key Pairs
+    """
+    
+    # exponent e shall be 16 < e < 2**256. FIPS 186-4. B.3.1
+    E = 65537   
 
     if bits not in (2048, 3072):
         print('Error. Use bits == 2048 or 3072')
         return 0, 0
-    
-    while True:    
-        p = getprime(bits//2)
-        q = getprime(bits//2)   
-        phi = (p - 1)*(q - 1)    
-        
-        # Verify that e and phi are comprime           
-        if gcd(E, phi) == 1:
-            n = p * q
-            d = pow(E, -1, phi)
-            public_key = (E, n)
-            private_key = (d, n)
-            return public_key, private_key    
+
+    #  all IFC keys shall follow criteria to conform to FIPS 186-4. B.3.1
+    nlen = bits//2    
+    lowlimit = (2**0.5) * (2**(nlen-1))
+    while True:        
+        p = getprime(nlen)
+        if  p >= lowlimit and gcd(E, p-1) == 1:
+            q = getprime(nlen)
+            if abs(p - q) > 2**(nlen-100):
+                if q >= lowlimit and gcd(E, q-1) == 1:                
+                    # (p–1) and (q–1) shall be relatively prime to e.
+                    phi = (p - 1)*(q - 1)            
+                    # Verify that e and phi are comprime.       
+                    if gcd(E, phi) == 1:
+                        n = p * q
+                        d = pow(E, -1, phi)
+                        # If d ≤ 2**(nlen//2), new p, q, d shall be determined.
+                        if 2**(nlen//2) < d < phi:
+                            public_key = (E, n)
+                            private_key = (d, n)
+                            return public_key, private_key 
